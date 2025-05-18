709af86bd0e9da9745d9e48ede636262eda9428b
Isidro Moroso Varona <isidromv79@gmail.com>
2025-05-18 12:48:49 +0200

    Add scatter plot NOK/Brent for correlation analysis

--- Oil Money project/Oil Money NOK.r
+++ Oil Money project/Oil Money NOK.r
@@ -15,3 +15,11 @@ df <- read.csv("data/brent crude nokjpy.csv", check.names = FALSE)
 colnames(df)[1] <- "date"
 df$date <- as.Date(df$date, format = "%m/%d/%Y")
 
+# Scatter plot NOK vs Brent
+df %>%
+  filter(date < as.Date("2018-04-24")) %>%
+  ggplot(aes(x = brent, y = nok)) +
+  geom_point(color = "#5f0f4e", size = 0.8) +
+  labs(title = "NOK Brent Correlation", x = "Brent in JPY", y = "NOKJPY") +
+  theme_minimal()
+
