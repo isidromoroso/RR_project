144fa54a3ecec2465e3a28d3bf7631b7b32fa6df
Isidro Moroso Varona <isidromv79@gmail.com>
2025-05-18 19:34:32 +0200

    Edit timeframe for Dataset variable in Oil Money NOK .py and .r, using last commit information

--- Oil Money project/Oil Money NOK.r
+++ Oil Money project/Oil Money NOK.r
@@ -389,7 +389,7 @@ plot_signals(signals_bt, "nok")
 graph_profit(portfolio_bt, "nok")
 
 # Grid search for parameters
-results <- expand.grid(h = 5:19, s = seq(0.3, 1.1, 0.05)) %>%
+results <- expand.grid(h = 5:19, s = seq(0.3, 1.05, 0.05)) %>%
   mutate(return = map2_dbl(h, s, ~{
     sig <- signal_generation(portfolio_data, "brent", "nok", oil_money, holding_threshold = .x, stop = .y)
     port <- portfolio(sig, "nok")
@@ -398,7 +398,7 @@ results <- expand.grid(h = 5:19, s = seq(0.3, 1.1, 0.05)) %>%
 
 # Trading returns distribution histogram (on NOK)
 ggplot(results, aes(x = return * 100)) +
-  geom_histogram(binwidth = 0.5, fill = "#f09e8c", color = "white") +
+  geom_histogram(binwidth = 0.50, fill = "#f09e8c", color = "white") +
   labs(title = "Distribution of Return on NOK Trading", x = "Return (%)", y = "Frequency") +
   theme_minimal()
 
