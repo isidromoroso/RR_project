82e27f3cccdc00ec47833a6ba5ec89335e7d1861
Isidro Moroso Varona <isidromv79@gmail.com>
2025-05-19 20:45:03 +0200

    Fix comment of Oil Money NOK.py for better reproducibility

--- Oil Money project/Oil Money NOK.py
+++ Oil Money project/Oil Money NOK.py
@@ -8,58 +8,7 @@ import matplotlib.pyplot as plt
 plt.ioff()                 # turn off interactive mode
 
 # In[1]:
-
-
-#i call it oil money
-#cuz its a statistical arbitrage on crude benchmark and petrocurrency
-#the inspiration came from an article i read
-#it suggested to trade on petrocurrency when the oil price went uprising 
-#plus overall volatility for forex market was low
-#the first thing is to build up a model to explore the causality
-#we split the historical dfs into two parts
-#one for the model estimation, the other for the model validation
-#we do a regression on estimation horizon
-#we use linear regression to make a prediction on ideal price level
-#we set up thresholds based on the standard deviation of residual
-#take one deviation above as the upper threshold
-#if the currency price breaches the upper threshold
-#take a short position as it is assumed to revert to its 'normal' price range soon
-#vice versa
-#so its kinda like bollinger bands
-
-#however, our regression is based on statistics
-#we still need to consider fundamental influence
-#what if the market condition has changed
-#in that case our model wont work any more
-#well,all models lose their creditability over the time
-#denote the price deviating two sigmas away from predicted value as model failure
-#which we should revert our positions
-#e.g. the price is two sigmas above our predicted value
-#we change our short to long since the market has changed its sentiment
-#there is probably hidden information in the uprising price
-#lets follow the trend and see where it ends
-
-#this idea sounds very silly
-#nobody actually does it or not that i know of
-#i just wanna to see if the idea would work
-#perhaps the idea would bring a huge loss
-#nonetheless, it turns out to be a big surprise!
-
-#first, we choose our currency norwegian krone
-#norway is one of the largest oil producing countries with floating fx regime
-#other oil producing countries such as saudi, iran, venezuela have their fx pegged to usd
-#russia is supposed to be a good training set
-#nevertheless, russia gets sanctioned by uncle sam a lot
-#we would see this in the next script
-# https://github.com/je-suis-tm/quant-trading/blob/master/Oil%20Money%20project/Oil%20Money%20RUB.py
-
-#after targetting at norwegian krone, we have to choose a currency to evaluate nok
-#take a look at norway's biggest trading partners 
-#we should include us dollar, euro and uk sterling as well as brent crude price in our model
-#in addition, the base currency would be japanese yen 
-#cuz its not a big trading partner with norway
-#which implies it doesnt have much correlation with nok
-#preparation is done, lets get started!
+# Importing libraries
 
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
@@ -72,6 +21,7 @@ from statsmodels.tsa.stattools import adfuller as adf
 
 
 # In[2]:
+# Load and preprocess the dataset
 
 df=pd.read_csv('data/brent crude nokjpy.csv')
 df.set_index(pd.to_datetime(df[list(df.columns)[0]]),inplace=True)
@@ -79,11 +29,11 @@ del df[list(df.columns)[0]]
 
 
 # In[3]:
+# Data visualitation and analysis
+# Data from 2013-4-25 to 2017-4-25 as estimation horizon/training set
+# Data from 2017-4-25 to 2018-4-25 as validation horizon/testing set
 
-#identification
-#first of first, using scatter plot to visualize the correlation
-#lets denote data from 2013-4-25 to 2017-4-25 as estimation horizon/training set
-#lets denote data from 2017-4-25 to 2018-4-25 as validation horizon/testing set
+# Scatter plot of NOKJPY vs Brent before 2023 (correlation plot)
 ax=plt.figure(figsize=(10,5)).add_subplot(111)
 ax.spines['top'].set_visible(False)
 ax.spines['right'].set_visible(False)
@@ -94,12 +44,7 @@ plt.xlabel('Brent in JPY')
 plt.ylabel('NOKJPY')
 plt.show()
 
-#if we run a covariance matrix on nok and brent, we got
-#np.corrcoef(df['nok'],df['brent'])
-#array([[1.        , 0.89681228],[0.89681228, 1.        ]])
-
-
-#dual axis plot
+# Dual-axis-plot function (we will use it to plot several graphs)
 def dual_axis_plot(xaxis,data1,data2,fst_color='r',
                     sec_color='b',fig_size=(10,5),
                    x_label='',y_label1='',y_label2='',
@@ -129,7 +74,7 @@ def dual_axis_plot(xaxis,data1,data2,fst_color='r',
     plt.title(title)
     plt.show()
     
-#nok vs ir
+# NOK vs Interest Rate plot
 dual_axis_plot(df.index,df['nok'],df['interest rate'],
                fst_color='#34262b',sec_color='#cb2800',
                fig_size=(10,5),x_label='Date',
@@ -137,7 +82,7 @@ dual_axis_plot(df.index,df['nok'],df['interest rate'],
                legend1='NOKJPY',legend2='Interest Rate',
                grid=False,title='NOK vs Interest Rate')
 
-#nok vs brent
+# NOK vs Brent in JPY plot
 dual_axis_plot(df.index,df['nok'],df['brent'],
                fst_color='#4f2d20',sec_color='#3feee6',
                fig_size=(10,5),x_label='Date',
@@ -145,9 +90,8 @@ dual_axis_plot(df.index,df['nok'],df['brent'],
                legend1='NOKJPY',legend2='Brent',
                grid=False,title='NOK vs Brent')
                
-#nok vs gdp
-#cuz gdp is released quarterly
-#we need to convert nok into quarterly data as well
+# NOK vs GDP yoy growth plot
+# We need to convert NOK into quarterly data because the gdp is released quarterly
 ind=df['gdp yoy'].dropna().index
 dual_axis_plot(df.loc[ind].index,
                df['nok'].loc[ind],
@@ -159,7 +103,7 @@ dual_axis_plot(df.loc[ind].index,
                grid=False,title='NOK vs GDP')
 
 
-#Now we do our linear regression
+# Linear Regression (OLS)
 x0=pd.concat([df['usd'],df['gbp'],df['eur'],df['brent']],axis=1)
 x1=sm.add_constant(x0)
 x=x1[x1.index<'2017-04-25']
@@ -170,13 +114,9 @@ print(model.summary(),'\n')
 
 
 # In[4]:
+# Elastic Net regression (ElasticNetCV function)
+# Multicollinearity is observed in OLS so we use Elastic Net regression
 
-
-#from the summary u can tell there is multicollinearity
-#the condition number is skyrocketing
-#alternatively, i can use elastic net regression to achieve the convergence
-#check the link below for more details
-# https://github.com/je-suis-tm/machine-learning/blob/master/coordinate%20descent%20for%20elastic%20net.ipynb
 m=en(alphas=[0.0001, 0.0005, 0.001, 0.01, 0.1, 1, 10],
      l1_ratio=[.01, .1, .5, .9, .99],  max_iter=5000).fit(x0[x0.index<'2017-04-25'], y)  
 print(m.intercept_,m.coef_)
@@ -184,38 +124,22 @@ print(m.intercept_,m.coef_)
 print("Best l1_ratio:", m.l1_ratio_) # Print Best l1_ratio and best alpha to reproduce same parameter in R
 print("Best alpha:", m.alpha_)
 
-#elastic net estimation results:
-#3.79776228406 [ 0.00388958  0.01992038  0.02823187  0.00050092]
+# Elastic net estimation results:
+# 3.79776228406 [ 0.00388958  0.01992038  0.02823187  0.00050092]
 
 
 # In[5]:
-
-
-#calculate the fitted value of nok
+# Calculate the fitted values of NOK
 df['sk_fit']=(df['usd']*m.coef_[0]+df['gbp']*m.coef_[1]+
                  df['eur']*m.coef_[2]+df['brent']*m.coef_[3]+m.intercept_)
 
 
 # In[6]:
-
-
-#getting the residual
+# Calculate the residuals of NOK
 df['sk_residual']=df['nok']-df['sk_fit']
 
-
-#one can always argue what if we eliminate some regressors
-#in econometrics, if adding extra variables do not decrease adjusted r squared
-#or worsen AIC, BIC
-#we should include more information as long as it makes sense
-
-
 # In[7]:
-
-
-#lets generate signals based on the elastic net
-#we set one sigma of the residual as thresholds
-#two sigmas of the residual as stop orders
-#which is common practise in statistics
+# Define trading signals based on residual standard deviation bands (based on Elastic Net results)
 upper=np.std(df['sk_residual'][df.index<'2017-04-25'])
 lower=-upper
 
@@ -234,18 +158,7 @@ signals['signals']=0
 # In[8]:
 
 
-#while doing a traversal
-#we apply the rules mentioned before
-#if actual price goes beyond upper threshold
-#we take a short and bet on its reversion process
-#vice versa
-#we use cumsum to make sure our signals only get generated
-#for the first time condions are met
-#when actual price hits the stop order boundary
-#we revert our positions
-#u may wonder whats next for breaking the boundary
-#well, we stop the signal generation algorithm
-#we need to recalibrate our model or use other trend following strategies
+# Signal generation logic: 2 sigmas strategy with long/short/exit with Stop Loss and Stop Profit
 
 index=list(signals.columns).index('signals')
 
@@ -276,9 +189,7 @@ for j in range(len(signals)):
 
 
 # In[9]:
-
-
-#next, we plot the usual positions as the first figure
+# Plot trading signals on NOK chart
 from datetime import datetime
 
 ax=plt.figure(figsize=(10,5)).add_subplot(111)
@@ -306,13 +217,8 @@ plt.show()
 
 
 # In[10]:
-
-
-#the second figure explores thresholds and boundaries for signal generation
-#we can see after 2017/11/15, nokjpy price went skyrocketing
-#as a data scientist, we must ask why?
-#is it a problem of our model identification
-#or the fundamental situation of nokjpy or oil changed
+# Plot actual vs fitted with confidence bands
+# After 2017/11/15, nokjpy price went skyrocketing
 
 ax=plt.figure(figsize=(10,5)).add_subplot(111)
 ax.spines['top'].set_visible(False)
@@ -334,21 +240,12 @@ plt.show()
 
 # In[11]:
 
-
-#if we decompose nokjpy into long term trend and short term random process
-#we could clearly see that brent crude price has dominated short term random process
-#so what changed the long term trend?
-#there are a few possible reasons
-#saudi and iran endorsed an extension of production caps on that particular date
-#donald trump got elected as potus so he would encourage a depreciated us dollar
-#which ultimately pushed up the oil price
+# Brent crude drives NOK/JPY's short-term moves,
+# while long-term shifts may stem from geopolitical events and U.S. policy changes.
 
 # In[12]:
+# Plot normalized price trends (for visual comparison)
 
-#lets normalize all prices by 100
-#its easy to see that nok follows euro
-#and economics explanation would be norway is in eea
-#its economy heavily relies on eu
 ax=plt.figure(figsize=(10,5)).add_subplot(111)
 ax.spines['top'].set_visible(False)
 ax.spines['right'].set_visible(False)
@@ -367,21 +264,7 @@ plt.show()
 
 
 # In[13]:
-
-
-#that still doesnt sound convincable
-#lets try cointegration test
-#academically we should use johansen test which works on multi dimensions
-#unfortunately, there is no johansen test in statsmodels (at the time i wrote this script)
-#well, here we go again
-#we have to use Engle-Granger two step!
-#salute to Engle, mentor of my mentor Gallo
-#to the nobel prize winner
-
-#im not gonna explain much here
-#if u have checked my other codes, u sould know
-#details are in pair trading session
-# https://github.com/je-suis-tm/quant-trading/blob/master/Pair%20trading%20backtest.py
+# Stationarity test on residuals using ADF (Augmented Dickey-Fuller)
 
 x2=df['eur'][df.index<'2017-04-25']
 x3=sm.add_constant(x2)
@@ -392,20 +275,11 @@ ero=model.resid
 print(adf(ero))
 print(model.summary())
 
-#(-2.5593457642922992, 0.10169409761939013, 0, 1030, 
-#{'1%': -3.4367147300588341, '5%': -2.8643501440982058, '10%': -2.5682662399849185}, -1904.8360920752475)
-#0.731199409071
-#unfortunately, the residual hasnt even reached 90% confidence interval
-#we cant conclude any cointegration from the test
-#still, from the visualization
-#we can tell nok and eur are somewhat correlated
-#our rsquared suggested euro has the power of 73% explanation on nok
+# We cant conclude any cointegration
 
 
 # In[14]:
-
-
-#then lets do a pnl analysis
+# Simulate trading strategy: backtest PnL (Initial capital 2000, each postiion of 10)
 capital0=2000
 positions=100
 portfolio=pd.DataFrame(index=signals.index)
@@ -416,16 +290,14 @@ portfolio['signals']=signals['signals']
 
 
 # In[15]:
-
+# Portfolio date range
 
 portfolio=portfolio[portfolio.index>'2017-10-01']
 portfolio=portfolio[portfolio.index<'2018-01-01']
 
 
 # In[16]:
-
-
-#we plot how our asset value changes over time
+# Plot portfolio performance and signal points
 ax=plt.figure(figsize=(10,5)).add_subplot(111)
 ax.spines['top'].set_visible(False)
 ax.spines['right'].set_visible(False)
@@ -451,40 +323,27 @@ plt.xlabel('Date')
 plt.show()
 
 
-#surprising when our model is valid for prediction
-#its difficult to make money from thresholds oscillating
-#when actual price goes beyond stop order boundary
-#that is basically the most profitable trade ever
-#best to follow up with a momentum strategy
-#maybe this is not a statistical arbitrage after all
-#the model is a trend following entry indicator
+# Though the model predicts well, profits arise mainly from momentum beyond stop thresholds, 
+# suggesting it's more a trend-following strategy than statistical arbitrage.
 
 
 # In[17]:
-#now lets construct a trend following strategy based on the previous strategy
-#call it oil money version 2 or whatever
-#here i would only import the strategy script as this is a script for analytics and visualization
-#the official trading strategy script is in the following link
-# https://github.com/je-suis-tm/quant-trading/blob/master/Oil%20Money%20project/Oil%20Money%20Trading%20backtest.py
+# Oil_money_trading_backtest improved trading strategy optimised
+# Import trading backtest strategy
 import oil_money_trading_backtest as om
 
 dataset = df # We use this code for the short period: .loc['2014-10-23':'2015-08-20'], period [387:600] in the backtest
 dataset.reset_index(inplace=True)
 
-#generate signals,monitor portfolio performance
-#plot positions and total asset
+# Generate signals,monitor portfolio performance
+# Plot positions and total asset
 signals=om.signal_generation(dataset,'brent','nok',om.oil_money)
 p=om.portfolio(signals,'nok')
 om.plot(signals,'nok')
 om.profit(p,'nok')
 
-#but thats not enough, we are not happy with the return
-#come on, 2 percent return?
-#i may as well as deposit the money into the current account 
-#and get 0.75% risk free interest rate
-#therefore, we gotta try different holding period and stop loss/profit point
-#the double loop is very slow, i almost wanna do it in julia
-#plz go get a coffee or even lunch and dont wait for it
+# Positive return but very low, just about 2%
+# Strategy optimizer, holding period and Stop Loss/Profit
 dic={}
 for holdingt in range(5,20):
     for stopp in np.arange(0.3,1.1,0.05):
@@ -500,11 +359,9 @@ profile=pd.DataFrame({'params':list(dic.keys()),'return':list(dic.values())})
 
 # In[18]:
 
-#plotting the distribution of return
-#in average the return is 2%
-#but we can get -6% and 6% as extreme values
-#we dont give a crap about average
-#we want the largest positive return
+# Distribution of return plot
+# Average return is 2%
+# -6% and 6% as extreme values
 
 ax=plt.figure(figsize=(10,5)).add_subplot(111)
 ax.spines['top'].set_visible(False)
@@ -521,10 +378,7 @@ plt.show()
 
 # In[19]:
 
-#plotting the heatmap of return under different parameters
-#try to find the optimal parameters to maximize the return
-
-#turn the dataframe into a matrix format first
+# Heatmap of holding period and Stop Loss/Profit return
 matrix=pd.DataFrame(columns= \
                     [round(i,2) for i in np.arange(0.3,1.1,0.05)])
 
@@ -539,7 +393,6 @@ for i in matrix.columns:
     matrix[i]=matrix[i].apply(float)
 
 
-#plotting
 fig=plt.figure(figsize=(10,5))
 ax=fig.add_subplot(111)
 sns.heatmap(matrix,cmap='gist_heat_r',square=True, \
@@ -551,10 +404,9 @@ plt.ylabel('Position Holding Period (days)\n')
 plt.title('Profit Heatmap\n',fontsize=10)
 plt.style.use('default')
 
-#it seems like the return doesnt depend on the stop profit/loss point
-#it is correlated with the length of holding period
-#the ideal one should be 9 trading days
-#as for stop loss/profit point could range from 0.6 to 1.05
+# Stop profit/loss point doesn't really affect (Best: from 0.6 to 1.05)
+# More correlated with the length of holding period (Best: 9 trading days)
+
 #In[20]
 # Download all outputted figures in the file
 import os
